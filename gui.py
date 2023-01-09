1e5ebaa94fdee3b973a7d213d7c8b39707fccbc9
Victor Vimbert-Guerlais <victor.vimbertguerlais@gmail.com>
2023-01-09 21:12:55 +0100

    Création du gui

--- gui.py
+++ gui.py
@@ -5,6 +5,9 @@ import tkinter as tk
 import use_csv
 import pyperclip
 from PIL import ImageTk, Image
+import shutil
+import os
+import glob
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -13,7 +16,6 @@ class App(customtkinter.CTk):
     WIDTH = 780
     HEIGHT = 550
     MAX_CONTACT_PER_PAGE = 11 #Nombre de contact par page de la liste de contact
-    PATH_ANNUAIRE = 'annuaire.csv'
     NB_RESULT_WIDHT = 4 #Largeur de la grille de resultat
     NB_RESULT_HEIGHT = 3 #Hauteur de la grille de resultat
     OPEN_NEW_ANNUAIRE_AT_LAUNCH = True
@@ -23,7 +25,7 @@ class App(customtkinter.CTk):
 
         self.contact_list = []
         
-        self.title("Simulation launcher")
+        self.title("Annuaire")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
 
         self.grid_columnconfigure((1, 2), weight=1)
@@ -46,16 +48,21 @@ class App(customtkinter.CTk):
 
         self.result_interface_frame.grid_columnconfigure(0, weight=1)
         self.result_interface_frame.grid_rowconfigure(0, weight=1)
+        
+        self.is_modification_window_open = False
 
         if App.OPEN_NEW_ANNUAIRE_AT_LAUNCH:
-            App.PATH_ANNUAIRE = filedialog.askopenfilename(filetypes=[('CSV file', '*.csv')])
-
-        self.contact_list = use_csv.read(App.PATH_ANNUAIRE)
-        self.create_contact_tab()
-        self.createSearchInterface()
-
+            path = filedialog.askopenfilename(filetypes=[('Annuaire file', '*.annuaire')])
+            self.unpackFile(path)
+            
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        
         self.mainloop()
 
+    def on_closing(self):
+        self.saveFile()
+        self.destroy()
+    
     def create_contact_tab(self):
         """
         Create the tab view on the left that serve to display a list of the contacts
@@ -94,7 +101,7 @@ class App(customtkinter.CTk):
 
     def createSearchInterface(self):
         """
-        Create the search interface
+        Create the search interface for the application. This includes a label, two entry fields for searching by name or number, and a button to initiate the search.
         """
         self.searchLabel = customtkinter.CTkLabel(master=self.search_interface_frame, text="Search", font=customtkinter.CTkFont(size=45, weight="bold"))
         self.searchLabel.grid(row=0, column=0, padx=30, pady=20)
@@ -150,71 +157,74 @@ class App(customtkinter.CTk):
         """
         Create a pop-up window wich can modifie or delete a contact
         """
-        self.current_contact_index = contact_index
+        if not self.is_modification_window_open:
+            self.is_modification_window_open = True
+            self.current_contact_index = contact_index
 
-        name:str = self.contact_list[contact_index].get('nom')
-        numero = self.contact_list[contact_index].get('numero')
-        img = None
-        if self.contact_list[contact_index].get('photo_name') != None:
-            photo_path = 'photos/'+self.contact_list[contact_index].get('photo_name')
-            img = customtkinter.CTkImage(Image.open(photo_path),
-                                            size=(200, 200))
+            name:str = self.contact_list[contact_index].get('nom')
+            numero = self.contact_list[contact_index].get('numero')
+            img = None
+            if self.contact_list[contact_index].get('photo_name') != 'None':
+                photo_path = 'working_directorie/'+self.contact_list[contact_index].get('photo_name')
+                img = customtkinter.CTkImage(Image.open(photo_path),
+                                                size=(200, 200))
+                
+
+            self.contact_modification_window = customtkinter.CTkToplevel()
+            self.contact_modification_window.title(f"Modifier le contact {name}")
+            self.contact_modification_window.geometry("730x230")
+
+            self.contact_modification_window.protocol("WM_DELETE_WINDOW", self.onModificationWindowClose)
+
+            self.contact_modification_window.grid_columnconfigure(1, weight=1)
+
+            self.contact_modification_window.grid_rowconfigure(0, weight=0)
+            self.contact_modification_window.grid_rowconfigure(1, weight=1)
+
+            self.left_frame_subWindow = customtkinter.CTkFrame(master=self.contact_modification_window, corner_radius=0, width=140)
+            self.left_frame_subWindow.grid(row=0, column=0, rowspan=2, padx=0, pady=0, sticky="nswe")
+            self.left_frame_subWindow.grid_rowconfigure(0, weight=1)
+            self.left_frame_subWindow.grid_rowconfigure(1, weight=0)
+
+            self.title_subWindow = customtkinter.CTkLabel(master=self.left_frame_subWindow, text=name.replace(' ', '\n'), font=customtkinter.CTkFont(size=25, weight="bold"))
+            self.title_subWindow.grid(padx=20, pady=20, sticky="n")
+
+            self.del_contact_button = customtkinter.CTkButton(master=self.left_frame_subWindow, text="Supprimer", command=self.deleteContact)
+            self.del_contact_button.grid(sticky="ews", padx=10, pady=10)
             
+            self.right_frame_subWindow = customtkinter.CTkFrame(master=self.contact_modification_window)
+            self.right_frame_subWindow.grid(row=0, column=1, rowspan=2, padx=15, pady=15, sticky="nswe")
+            self.right_frame_subWindow.grid_columnconfigure((0, 1), weight=1)
+            self.right_frame_subWindow.grid_rowconfigure((0, 2), weight=0)
+            self.right_frame_subWindow.grid_rowconfigure(1, weight=1)
 
-        self.contact_modification_window = customtkinter.CTkToplevel()
-        self.contact_modification_window.title(f"Modifier le contact {name}")
-        self.contact_modification_window.geometry("730x230")
+            self.label_modification = customtkinter.CTkLabel(master=self.right_frame_subWindow, text="Modification", font=customtkinter.CTkFont(size=20, weight="bold"))
+            self.label_modification.grid(row=0, column=0, sticky="ws", padx=15, pady=10)
 
-        self.contact_modification_window.protocol("WM_DELETE_WINDOW", self.onModificationWindowClose)
+            self.name_current_contact = customtkinter.StringVar()
+            self.name_current_contact.set(name)
+            self.name_entry_modification = customtkinter.CTkEntry(master=self.right_frame_subWindow, textvariable=self.name_current_contact)
+            self.name_entry_modification.grid(row=1, column=0, padx=15, pady=15, sticky='ws')
 
-        self.contact_modification_window.grid_columnconfigure(1, weight=1)
-
-        self.contact_modification_window.grid_rowconfigure(0, weight=0)
-        self.contact_modification_window.grid_rowconfigure(1, weight=1)
-
-        self.left_frame_subWindow = customtkinter.CTkFrame(master=self.contact_modification_window, corner_radius=0, width=140)
-        self.left_frame_subWindow.grid(row=0, column=0, rowspan=2, padx=0, pady=0, sticky="nswe")
-        self.left_frame_subWindow.grid_rowconfigure(0, weight=1)
-        self.left_frame_subWindow.grid_rowconfigure(1, weight=0)
-
-        self.title_subWindow = customtkinter.CTkLabel(master=self.left_frame_subWindow, text=name.replace(' ', '\n'), font=customtkinter.CTkFont(size=25, weight="bold"))
-        self.title_subWindow.grid(padx=20, pady=20, sticky="n")
-
-        self.del_contact_button = customtkinter.CTkButton(master=self.left_frame_subWindow, text="Supprimer", command=self.deleteContact)
-        self.del_contact_button.grid(sticky="ews", padx=10, pady=10)
-        
-        self.right_frame_subWindow = customtkinter.CTkFrame(master=self.contact_modification_window)
-        self.right_frame_subWindow.grid(row=0, column=1, rowspan=2, padx=15, pady=15, sticky="nswe")
-        self.right_frame_subWindow.grid_columnconfigure((0, 1), weight=1)
-        self.right_frame_subWindow.grid_rowconfigure((0, 2), weight=0)
-        self.right_frame_subWindow.grid_rowconfigure(1, weight=1)
-
-        self.label_modification = customtkinter.CTkLabel(master=self.right_frame_subWindow, text="Modification", font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.label_modification.grid(row=0, column=0, sticky="ws", padx=15, pady=10)
-
-        self.name_current_contact = customtkinter.StringVar()
-        self.name_current_contact.set(name)
-        self.name_entry_modification = customtkinter.CTkEntry(master=self.right_frame_subWindow, textvariable=self.name_current_contact)
-        self.name_entry_modification.grid(row=1, column=0, padx=15, pady=15, sticky='ws')
-
-        self.numero_current_contact = customtkinter.StringVar()
-        self.numero_current_contact.set(numero)
-        self.name_entry_modification = customtkinter.CTkEntry(master=self.right_frame_subWindow, textvariable=self.numero_current_contact)
-        self.name_entry_modification.grid(row=2, column=0, padx=15, pady=15, sticky='wn')
-        
-        self.accept_button_save_changes = customtkinter.CTkButton(self.right_frame_subWindow, text="Save",width=60, command=lambda:self.save_contact_modification())
-        self.accept_button_save_changes.grid(row=2, column=1, padx=15, pady=15, sticky='es')
-
-        self.photo_frame = customtkinter.CTkFrame(master=self.contact_modification_window, width=200)
-        self.photo_frame.grid(row=0, column=2, padx=15, pady=15)
-        if img != None:
-            self.photo_label = customtkinter.CTkLabel(self.photo_frame, image=img, text="")
-            self.photo_label.grid()
+            self.numero_current_contact = customtkinter.StringVar()
+            self.numero_current_contact.set(numero)
+            self.name_entry_modification = customtkinter.CTkEntry(master=self.right_frame_subWindow, textvariable=self.numero_current_contact)
+            self.name_entry_modification.grid(row=2, column=0, padx=15, pady=15, sticky='wn')
             
-        else:
-            self.add_img_button = customtkinter.CTkButton(master=self.photo_frame, text="Add Photo")
-            self.add_img_button.grid(padx=30, pady=87)
-        self.contact_modification_window.update()
+            self.accept_button_save_changes = customtkinter.CTkButton(self.right_frame_subWindow, text="Save",width=60, command=lambda:self.save_contact_modification())
+            self.accept_button_save_changes.grid(row=2, column=1, padx=15, pady=15, sticky='es')
+
+            self.photo_frame = customtkinter.CTkFrame(master=self.contact_modification_window, width=200)
+            self.photo_frame.grid(row=0, column=2, padx=15, pady=15)
+            if img != None:
+                self.photo_label = customtkinter.CTkLabel(self.photo_frame, image=img, text="")
+                self.photo_label.grid()
+                
+            else:
+                self.add_img_button = customtkinter.CTkButton(master=self.photo_frame, text="Add Photo", command=self.addImage)
+                self.add_img_button.grid(padx=30, pady=87)
+            self.contact_modification_window.update()
+            self.contact_modification_window.grab_set()
 
     def onModificationWindowClose(self):
         """
@@ -222,6 +232,7 @@ class App(customtkinter.CTk):
         """
         self.contact_modification_window.destroy()
         self.redrawInterface()
+        self.is_modification_window_open = False
 
     def verifieChanges(self):
         """
@@ -241,8 +252,8 @@ class App(customtkinter.CTk):
         Reset the result tab and redraw the inteface
         """
         self.result_tab=None
-        self.searchButtonFunc()
         self.create_contact_tab()
+        self.createSearchInterface()
 
     def save_contact_modification(self):
         if self.verifieChanges():
@@ -252,7 +263,7 @@ class App(customtkinter.CTk):
             self.contact_list[self.current_contact_index]['nom'] = new_name
             self.contact_list[self.current_contact_index]['numero'] = new_numero
 
-            annuaire.saveChanges(App.PATH_ANNUAIRE, self.contact_list)
+            annuaire.saveChanges("working_directorie/contact_list.csv", self.contact_list)
             self.onModificationWindowClose()
 
     def searchButtonFunc(self):
@@ -272,15 +283,75 @@ class App(customtkinter.CTk):
         
 
     def deleteContact(self):
+        """
+        Remove the currently selected contact from the list of contacts and save the changes to the CSV file.
+        """
         self.contact_list.pop(self.current_contact_index)
-        annuaire.saveChanges(App.PATH_ANNUAIRE, self.contact_list)
+        annuaire.saveChanges("working_directorie/contact_list.csv", self.contact_list)
         self.onModificationWindowClose()
     
     def addContactButton(self):
-        new_contact= {"nom":"default", "numero":"None"}
+        """
+        Add a new default contact to the list of contacts and open the contact modification window for the new contact.
+        """
+        new_contact= {"nom":"default", "numero":"None", "photo_name":"None"}
         self.contact_list.append(new_contact)
         self.create_contact_modification_window(len(self.contact_list)-1)
+    
+    def addImage(self):
+        image_path = filedialog.askopenfilename(title="Selectionez un image")
+        save_folder = "working_directorie"
+        # Open the image using PIL
+        image = Image.open(image_path)
+        
+        # Crop the image to 200x200 pixels
+        image.thumbnail(size=(200, 200))
+        
+        # Create the save folder if it does not exist
+        if not os.path.exists(save_folder):
+            os.makedirs(save_folder)
+        
+        # Get the file name and extension of the image
+        file_name, file_extension = os.path.splitext(os.path.basename(image_path))
+        save_path = os.path.join(save_folder, file_name + file_extension)
+        image.save(save_path)
+        image.close()
+        
+        self.contact_list[self.current_contact_index]['photo_name'] = file_name + file_extension
+        
+        self.onModificationWindowClose()
+        self.create_contact_modification_window(self.current_contact_index)
+    
+    def saveFile(self):
+        output_filename = filedialog.asksaveasfilename(filetypes=[('Annuaire file', '*.annuaire')]).split(".")[0]
+        
+        #Delete possibly remaining .zip files
+        dir = os.listdir("working_directorie")
 
-
+        for file in dir:
+            if file.endswith(".zip"):
+                os.remove(os.path.join("working_directorie", file))
+        
+        shutil.make_archive(output_filename, 'zip', "working_directorie")
+        os.replace(f"{output_filename}.zip", f"{output_filename}.annuaire")
+        
+        files = glob.glob('working_directorie/*')
+        for f in files:
+            os.remove(f)
+            
+    def unpackFile(self, path):
+        new_path = shutil.copy(path, 'working_directorie')
+        new_name = new_path.split(".")[0]+".zip"
+        os.rename(new_path, new_name)
+        try:
+            shutil.unpack_archive(new_name, 'working_directorie')
+        except:
+            pass
+        csv_path = 'working_directorie/contact_list.csv'
+        self.contact_list = use_csv.read(csv_path)
+        self.redrawInterface()
+        
+        
+        
 
 app=App()
